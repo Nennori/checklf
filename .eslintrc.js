6a9288d6d1612addadef3e0f23b231c27cff589f
Anastasiya Zhuk <zhukad@aldera-soft.ru>
2021-11-10 22:25:52 +0700

    feat: add configs

---
+++ .eslintrc.js
@@ -0,0 +1,83 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  plugins: ['import', 'jsx-a11y', 'react', 'prettier', '@typescript-eslint', 'import'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'jest-enzyme',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 8,
+    requireConfigFile: false,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'linebreak-style': 'off',
+    'import/no-unresolved': [2, { commonjs: true, amd: true }],
+
+    'arrow-parens': 'off',
+    'object-curly-newline': 'off',
+    'no-mixed-operators': 'off',
+    'arrow-body-style': 'off',
+    'function-paren-newline': 'off',
+    'no-plusplus': 'off',
+    'space-before-function-paren': 0,
+
+    'max-len': ['error', 100, 2, { ignoreUrls: true }],
+    'no-console': 'error',
+    'no-alert': 'error',
+
+    'no-param-reassign': 'off',
+    radix: 'off',
+
+    'react/require-default-props': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+
+    'prefer-destructuring': 'off',
+
+    'react/no-find-dom-node': 'off',
+    'react/no-did-mount-set-state': 'off',
+    'react/no-unused-prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+
+    'jsx-a11y/anchor-is-valid': ['error', { components: ['Link'], specialLink: ['to'] }],
+    'jsx-a11y/label-has-for': [
+      2,
+      {
+        required: {
+          every: ['id'],
+        },
+      },
+    ],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'prettier/prettier': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
+};
